20bdcb021df247d3226f46dac3841bf9e233660f
Ryan Hansberry <rhansby@gmail.com>
2013-07-27 10:12:15 -0700

    Implemented saving to imgur functionality

--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,27 @@ var marissafy = function(coords) {
     almighty_marissa_face.src = 'img/marissa_1.png';
 }
 
+var save_to_imgur = function() {
+    var canvas_shot = canvas.toDataURL('image/jpeg').toString()
+    canvas_shot = canvas_shot.substr(23, canvas_shot.length - 23); // cut out first 23 characters for imgur to accept it
+
+    $.ajax( {
+        url: 'https://api.imgur.com/3/image',
+        type: 'post',
+        data: {
+            image: canvas_shot
+        },
+        headers: {
+            Authorization: 'Client-ID 74e4bd4738704d8'
+        },
+        dataType: 'json',
+        success: function(response) {
+            console.log(response.data.link);
+            // TODO: Write response.data.link to box
+        }
+    });
+}
+
 $(document).ready(function() {
     $('#upload-form').ajaxForm(function(response) {
         if ( response && response !== '' ) {
