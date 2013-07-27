27d95e16ec1021204bbb9d9acc22a7e098cd035b
Ryan Hansberry <rhans@glossaryonce-lm.(none)>
2013-07-26 20:22:34 -0700

    Got upload code working

--- web.js
+++ web.js
@@ -1,22 +1,6 @@
-/*
-var express = require("express");
-var app = express();
-app.use(express.logger());
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(request, response) {
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log("Listening on " + port);
-}); */
-
 var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
-
 
 var handle = {};
 handle["/"] = requestHandlers.start;
