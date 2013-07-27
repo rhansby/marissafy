e211342fa227a9ff1118d16baf8c8ca35dfe15c6
emi <emin@yahoo-inc.com>
2013-07-27 10:50:01 -0700

    new photos

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,26 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
+var upload_image; 
 
 var marissafy = function(coords) {
-    var almighty_marissa_face = new Image();
+
+    var almighty_marissa_face = new Image(),
+        marissa_faces = ['img/marissa_1.png', 'img/marissa_3.png', 'img/marissa_4.png','img/marissa_5.png', 'img/marissa_6.png'],
+        random_face = Math.floor(Math.random()*6);
+
     almighty_marissa_face.onload = function() {
-        context.drawImage(almighty_marissa_face, coords.x, coords.y, coords.width, coords.height);
+        context.drawImage(almighty_marissa_face, coords.x, (coords.y * .9), (coords.width * 1.25), (almighty_marissa_face.height*1.25));
     };
-    almighty_marissa_face.src = 'img/marissa_1.png';
+
+    console.log(random_face);
+    almighty_marissa_face.src = marissa_faces[random_face]; //'img/marissa_1.png';
 }
 
 $(document).ready(function() {
     $('#upload-form').ajaxForm(function(response) {
         if ( response && response !== '' ) {
             coords_array = JSON.parse(response);
+            context.drawImage(upload_image, 0, 0, canvas.width, canvas.height);
             for (var i = 0; i < coords_array.length; i++) {
                 marissafy(coords_array[i]);
             }
@@ -32,6 +40,7 @@ $(document).ready(function() {
             canvas.width = this.width / 2;
             canvas.height = this.height / 2;
             context.drawImage(this, 0, 0, canvas.width, canvas.height);
+            upload_image = this; 
         };
         image.src = image_location;
     });
